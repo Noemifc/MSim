358ccd4037c6e02cdb65f06e61e246f287db2ecf
Noemifc <noemifico@gmail.com>
2025-09-22 16:09:40 -0500

    Update golden.py

--- examples/golden.py
+++ examples/golden.py
@@ -1,26 +1,40 @@
+
 from msim.interface import XRayScanner
 import numpy as np
 
-scanner = XRayScanner("config.json")
-scanner.load_volume("phantom.zarr", "phantom.json")
-
-#def golden angle 
-golden_a =180(3-np.sqrt(5))/2 # deg
-golden_a_rad = golden_a * np.pi / 180  # rad
-
-# select projections
-num_proj = 360    #select number of projections
-theta_start = 30  # start angle deg
-
+#setup scanner 
+scanner = XRayScanner("enhanced_config.json")
+scanner.load_volume("phantom_bone.zarr", "phantom_bone.json")
 
 #golden angles array 
-golden_angles_tomo = np.mod(theta_start + np.arange(num_proj) * golden_a, 180)     # generate golden-angle from theta_start  in mod 180 , function numpy.arange([start, ]stop, [step, ]dtype=None)
-#golden_angles_tomo_sorted = np.sort( golden_angles_tomo)  # if you need to rearrange 
+golden_a = 180*(3 - np.sqrt(5)) / 2  # deg
+golden_a_rad = golden_a*np.pi / 180  # rad 
 
-# Custom GOLDEN angle sequence
-golden_angles_tomo = np.mod(theta_start + np.arange(num_proj) * golden_a, 180)  
+num_proj = 360     #select number of projections , int
+theta_start = 30   # start angle deg
+
+golden_angles_tomo = np.mod(theta_start + np.arange(num_proj) * golden_a, 180)
+
+print("Primi 10 angoli (float):", golden_angles_tomo[:10])
+
+# scan golden-angle
 projections, dose_stats = scanner.tomography_scan(
-    golden_angles_tomo, 
-     "golden_tomo_with_dose.h5", # PREDE QUESTO FILE O LO GENNERA? SE LO GENERA CAMBIA NOME IN "golden_tomo_with_dose.h5"
-    calculate_dose=fALSE
+    golden_angles_tomo,
+    "golden_tomo_with_dose.h5",
+    calculate_dose=False
 )
+
+# golden-angle sorted 
+golden_angles_tomo_sorted = np.sort(golden_angles_tomo) #golden angles sorted
+
+print("Primi 10 angoli ordinati:", golden_angles_tomo_sorted[:10])
+
+# scan golden-angle sorted 
+
+projections_sorted, dose_stats_sorted = scanner.tomography_scan(
+    golden_angles_tomo_sorted,
+    "golden_angles_tomo_sorted.h5",
+    calculate_dose=False
+)
+
+
