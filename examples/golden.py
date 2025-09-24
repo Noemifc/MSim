a36e817d8c07a872f97720d284d850f3ba13c76a
Noemifc <noemifico@gmail.com>
2025-09-24 11:16:51 -0500

    Update golden.py

--- examples/golden.py
+++ examples/golden.py
@@ -1,26 +1,48 @@
+#!/usr/bin/env python3
+"""
+Golden-angle tomography example with automatic phantom generation.
+"""
+#--------------------------------------------
 import numpy as np
-import json
-import os 
-import z5py
 import sys
+import os 
+#import z5py
+#import json
 
-from msim.simulator import XRayScanner, quick_tomography , analyze_dose_only
-#export LD_LIBRARY_PATH=/local/cuda-12.0/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
+# path MSIM per moduli import
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from msim.simulator import XRayScanner
+#--------------------------------------------
+#generate phantom if doesn't exist ( sostituire )
+phantom_file = "phantom_bone.zarr"
+phantom_json = "phantom_bone.json"
+
+if not os.path.exists(phantom_file):
+    print("Phantom non trovato. Generazione automatica in corso...")
+    from msim.generate_phantom import generate_phantom
+    generate_phantom(
+        "bone",
+        shape=(64, 96, 96),
+        voxel_size=(0.5, 0.5, 0.5)
+    )
+    print("Phantom generato correttamente!")
+
+#--------------------------------------------
 #setup scanner 
 scanner = XRayScanner("enhanced_config.json")
 scanner.load_volume("phantom_bone.zarr", "phantom_bone.json")
 
+#--------------------------------------------
 #golden angles array 
+
 golden_a = 180*(3 - np.sqrt(5)) / 2  # deg
 golden_a_rad = golden_a*np.pi / 180  # rad 
-
 num_proj = 360     #select number of projections , int
 theta_start = 30   # start angle deg
 
 golden_angles_tomo = np.mod(theta_start + np.arange(num_proj) * golden_a, 180)
-
-print("Primi 10 angoli (float):", golden_angles_tomo[:10])
+print(" 10 golden angles (float):", golden_angles_tomo[:10])
 
 # scan golden-angle
 projections, dose_stats = scanner.tomography_scan(
@@ -31,15 +53,13 @@ projections, dose_stats = scanner.tomography_scan(
 
 # golden-angle sorted 
 golden_angles_tomo_sorted = np.sort(golden_angles_tomo) #golden angles sorted
-
-print("Primi 10 angoli ordinati:", golden_angles_tomo_sorted[:10])
+print("10 angles sorted", golden_angles_tomo_sorted[:10])
 
 # scan golden-angle sorted 
-
 projections_sorted, dose_stats_sorted = scanner.tomography_scan(
     golden_angles_tomo_sorted,
     "golden_angles_tomo_sorted.h5",
     calculate_dose=False
 )
-
+print("Golden-angle scans complete")
 
